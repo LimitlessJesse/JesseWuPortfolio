8531f0c41fa7cd9d39b2b0dc66c856fb490a829c
LimitlessJesse <jessewu1999@gmail.com>
2024-09-04 15:15:29 -0500

    added background

--- jesse-wu-portfolio/app/(home)/page.tsx
+++ jesse-wu-portfolio/app/(home)/page.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Intro from './components/Intro'
+import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams";
 
 export default function page() {
   return (
-    <div className="min-h-screen">
-      <Navbar />
-      <div className="max-w-7xl mx-auto">
-        <Intro />
+    
+      <div className="min-h-screen">
+        <Navbar />
+        <BackgroundBeamsWithCollision>
+          <div className="max-w-7xl mx-auto">
+          <Intro />
+        </div>
+        </BackgroundBeamsWithCollision>       
       </div>
-    </div>
+    
   )
 }
